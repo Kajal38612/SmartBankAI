09f94e8495ba83a46b8a6d10bf7eeeb8a1183561
Kajal Kumari <155719010+Kajal38612@users.noreply.github.com>
2026-08-11 20:03:54 +0530

    Update UserController.java

--- src/main/java/com/smartbank/smartbankai/controller/UserController.java
+++ src/main/java/com/smartbank/smartbankai/controller/UserController.java
@@ -3,6 +3,7 @@ package com.smartbank.smartbankai.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.smartbank.smartbankai.dto.LoginRequest;
 import com.smartbank.smartbankai.dto.LoginResponse;
