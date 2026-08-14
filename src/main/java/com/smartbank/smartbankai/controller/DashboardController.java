ca80e3b19f4f1dc2a9802a99b793130d9792febb
Kajal Kumari <155719010+Kajal38612@users.noreply.github.com>
2026-08-14 20:23:10 +0530

    Update DashboardController.java

--- src/main/java/com/smartbank/smartbankai/controller/DashboardController.java
+++ src/main/java/com/smartbank/smartbankai/controller/DashboardController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.smartbank.smartbankai.dto.DashboardResponse;
 import com.smartbank.smartbankai.entity.Account;
@@ -15,6 +16,7 @@ import com.smartbank.smartbankai.repository.UserRepository;
 import com.smartbank.smartbankai.security.JwtService;
 import com.smartbank.smartbankai.service.DashboardService;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/dashboard")
 public class DashboardController {
