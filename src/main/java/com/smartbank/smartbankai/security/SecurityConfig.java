a221808ac957544f21ee564dd68e15fd096926e3
Kajal Kumari <155719010+Kajal38612@users.noreply.github.com>
2026-08-15 16:24:42 +0530

    Update SecurityConfig.java

--- src/main/java/com/smartbank/smartbankai/security/SecurityConfig.java
+++ src/main/java/com/smartbank/smartbankai/security/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig {
                                 "/transaction/**",
                                 "/dashboard/**"
                         )
-                        .hasAuthority("USER")
+                        .hasAuthority("USER", "ADMIN")
 
                         // User Profile APIs
                         .requestMatchers(
@@ -80,7 +80,7 @@ public class SecurityConfig {
                                 "/user/update/**",
                                 "/user/delete/**"
                         )
-                        .hasAuthority("USER")
+                        .hasAuthority("USER", "ADMIN")
 
                         // Everything else
                         .anyRequest()
